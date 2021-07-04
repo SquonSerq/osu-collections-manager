9c2e79cf3d05ce7c549ece9adbe43df48d7c9746
SquonSerq <squonserq@bk.ru>
2021-07-04 22:59:19 +0600

    add main hash solvers,

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const fs = require('fs')
+const osuDb = require('osudb')
+const md5File = require('md5-file')
 
 function createWindow () {
   // Create the browser window.
@@ -16,7 +19,17 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
+}
+
+function readCollection(path, configName){
+  osuDb.readCollectionDB(path+"/collection.db", (data)=>{
+    fs.writeFileSync('./'+configName+'.json', JSON.stringify(data))
+  })
+}
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
 }
 
 // This method will be called when Electron has finished
@@ -36,8 +49,59 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
+  if (!fs.existsSync(tmpFilesDir)){
+    fs.rmdirSync(tmpFilesDir, { recursive: true })
+  }
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+tmpFilesDir = "./tmp"
+if (!fs.existsSync(tmpFilesDir)){
+  fs.mkdirSync(tmpFilesDir)
+}
+configLoad = JSON.parse(fs.readFileSync("./config.json", 'utf-8'))
+osuPath = configLoad["osu-path"]
+collectionFileName = configLoad["collections-file-name"]
+
+readCollection(osuPath, tmpFilesDir+"/"+collectionFileName)
+
+osuRegExp = /.*\.osu/
+osuSongsDir = fs.readdirSync(osuPath+"/Songs")
+songsHashes = new Object()
+diffsHashes = new Object()
+
+for(const [key, songDirectory] of Object.entries(osuSongsDir)){
+  if (fs.lstatSync(osuPath+"\\Songs\\"+songDirectory).isDirectory()){
+    songDirPath = fs.readdirSync(osuPath+"\\Songs\\"+songDirectory)
+    diffsArray = []
+    for(const [k, fileName] of Object.entries(songDirPath)){
+      if (osuRegExp.test(fileName)){
+        tmpHash = new Object()
+        tmpHash[fileName.substring(0, fileName.length-4)] = md5File.sync(osuPath+"\\Songs\\"+songDirectory+"\\"+fileName) 
+        diffsArray.push(tmpHash)
+        diffsHashes[fileName.substring(0, fileName.length-4)] = md5File.sync(osuPath+"\\Songs\\"+songDirectory+"\\"+fileName) 
+        console.log(fileName)
+      }
+    }
+    songsHashes[songDirectory] = diffsArray
+  }
+}
+
+collectionWithNames = new Object()
+collectionData = JSON.parse(fs.readFileSync(tmpFilesDir+"/"+collectionFileName+".json"))
+
+for (const [categoryName, categoryHashes] of Object.entries(collectionData)){
+  songNamesArray = []
+  categoryHashes.forEach(element => {
+    songNamesArray.push(getKeyByValue(diffsHashes, element))
+  });
+  collectionWithNames[categoryName] = songNamesArray
+}
+
+console.log(collectionWithNames)
+
+
+fs.writeFileSync(tmpFilesDir+"/"+"songsHashes.json", JSON.stringify(songsHashes))
+fs.writeFileSync(tmpFilesDir+"/"+"diffsHashes.json", JSON.stringify(diffsHashes))
+
+
--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+const fs = require('fs')
+
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -9,4 +10,24 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+
+  osuSongs = JSON.parse(fs.readFileSync("./tmp/songsHashes.json"))
+
+  for (const [mapName, v] of Object.entries(osuSongs)){
+    divMap = document.createElement("div")
+    divMap.innerHTML = "<p>"+mapName+"</p>"
+
+    v.forEach(el => {
+      diffName = Object.keys(el)[0]
+      divDiff = document.createElement("div")
+      divDiff.innerHTML = "<p class='diff'>"+diffName+"</p>"
+      divMap.appendChild(divDiff)
+
+    })
+
+
+    document.getElementById("maps").appendChild(divMap)
+  }
+
+
 })
